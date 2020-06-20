b593e828b642f1c16602379c616b2bde6d8f3f6b
Tedhoon <gt0305@likelion.org>
2020-06-20 19:36:12 +0900

    gui

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,3 +1,4 @@
+'''
 from tkinter import *
 win = Tk()
 win.title('모달 제목')
@@ -5,3 +6,25 @@ win.geometry("200x200+300+30")
 btn = Button(win, text="버튼")
 btn.place(x=20, y=20)
 win.mainloop()
+'''
+
+from tkinter import *
+
+root = Tk()
+
+
+lb = Label(root, text="이름")
+lb.pack()
+ 
+txt = Entry(root)
+txt.pack()
+ 
+btn = Button(root, text="OK")
+btn.pack()
+
+
+root.mainloop()
+# 기본 실행
+
+
+
